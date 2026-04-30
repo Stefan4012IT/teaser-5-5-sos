ca95266107402a2b3231987e60124d50c60938ed
stefan4012it <stefanra555@gmail.com>
2026-04-30 12:53:25 +0200

    ready

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import CircularMenu from './components/CircularMenu'
 import LeadForm from './components/LeadForm'
 
 import imgDesk from './assets/images/5-5-savremena_osnovna_hero-desk.jpg'
-// import imgMobile from './assets/images/5-5-savremena_osnovna_hero-mobile.jpg'
+import imgMobile from './assets/images/5-5-savremena_osnovna_hero-mob-img.png'
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
@@ -34,11 +34,17 @@ function App() {
           />
         </picture>
         <div className="hero__content">
-          <p className="hero__eyebrow">5.5. / Savremena</p>
+          <p className="hero__eyebrow">5.5. / Poklon za 5+</p>
 
           <h1 className="hero__title">
             5.5.&nbsp; je vaš dan odluke za čistu peticu!
           </h1>
+          <img
+            className="hero__mobile-image"
+            src={imgMobile}
+            alt=""
+            aria-hidden="true"
+          />
 
           <p className="hero__text">
             Ukoliko želite svom detetu da obezbedite najsvaremenenije obrazovanje ovo je pravi trenutak, <span className='boldeto'>imamo za vas iznenadjenje za 5+.</span>
